0c9a034216dc4eb07a90e37c1caba393628e30f8
mamadousankaraaa <abouanne393@gmail.com>
2026-06-13 01:04:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,37 @@ const app = express();
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("Serveur Roblox ID Finder actif !");
+app.get("/user/:username", async (req, res) => {
+    try {
+        const response = await fetch(
+            "https://users.roblox.com/v1/usernames/users",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    usernames: [req.params.username],
+                    excludeBannedUsers: false
+                })
+            }
+        );
+
+        const data = await response.json();
+
+        if (!data.data || !data.data.length) {
+            return res.status(404).json({
+                error: "Utilisateur introuvable"
+            });
+        }
+
+        res.json(data.data[0]);
+
+    } catch (err) {
+        res.status(500).json({
+            error: "Erreur serveur"
+        });
+    }
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Serveur lancé");
-});
+app.listen(process.env.PORT || 3000);
